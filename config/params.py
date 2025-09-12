c40c29e1c1a908ce08d7103b4de83e06b5f47cb3
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2025-09-12 14:18:42 -0500

    Update params.py

--- config/params.py
+++ config/params.py
@@ -72,7 +72,7 @@ class parameters:
         self.rangos_strikes = [[0,0.5] ] #escoge siempre el mas pegado al primero
         # self.rangos_strikes = [[2, 2.3], [2.15, 2.55], [2.4, 3]]
         self.diff_days_exp = 37
-        self.days_max = [21, 25]
+        self.days_max = [21, 29]
 
 
 
