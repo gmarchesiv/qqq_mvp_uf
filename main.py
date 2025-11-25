6daf09743f1b671014984819f010faa83fbbd1cc
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2025-11-25 12:01:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,6 @@ def main():
         if vars.fecha != now:
             clean_vars(vars,varsApp)
             data_option_open(app,vars,params)
-            
             generar_label(params, varsLb,app)
 
             timeNow = datetime.now(params.zone).time()
@@ -185,7 +184,7 @@ def main():
                varsApp.flag_bloqueo_tiempo=True
         else:
             load_app_vars(app, varsApp)
-        print(varsLb.ret_1H_back,"e")
+  
         wallet_config(app, params, vars)
 
         # sendStart(app, params)
@@ -202,7 +201,7 @@ def main():
         del dia y se rompe al finalizar el trading day. 
         '''
         #---------------------------------------------------
-        print(varsLb.ret_1H_back,"da")
+
         while True:
 
             timeNow = datetime.now(params.zone).time()
@@ -220,12 +219,9 @@ def main():
             '''
             #---------------------------------------------------
             # GENERAR LABEL
-
             if (timeNow.minute % 10 == 0 or timeNow.minute % 10 == 5):
-                
                 if varsLb.flag_minuto_label:
-                    print(varsLb.ret_1H_back,"c")
-                    generar_label(params, vars,app)
+                    generar_label(params, varsLb,app)
                     vars.flag_minuto_label=False
                     time.sleep(0.5)
                     # calculations(app, vars, params)
@@ -288,7 +284,7 @@ def main():
                 '''
                 #---------------------------------------------------
                 if int(timeNow.second) in params.frecuencia_accion:
-                    print(varsLb.ret_1H_back,"b")
+                    
                     # saveTransaction(app, params, vars)  # VERIFICADOR DE TRANSACCIONES
                     calculations(app, vars,varsBc, params)  # CALCULOS DE RUTINA
                     # readIBData(app, vars,varsLb)  # LOGS DE LOS CALCULOS
@@ -312,7 +308,7 @@ def main():
                     # ================================
  
                     registration(app, vars,varsApp, varsLb,params)
-                    print(varsLb.ret_1H_back,"a")
+                
                     time.sleep(0.5)
     
                 # ==================================
