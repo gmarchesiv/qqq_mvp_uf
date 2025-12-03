a8939f6ab44734d2c2e1a933bd2ca607ee016627
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2025-12-03 15:22:05 -0500

    Update params.py

--- config/params.py
+++ config/params.py
@@ -76,9 +76,9 @@ class parameters:
  
         # SELECCION DE STRIKES 
         self.rangos_strikes = [[1.9, 2.6] ]
-        self.days_min_exp = 30  # DIAS para el exp minimo de busqueda
+        self.days_min_exp = 27  # DIAS para el exp minimo de busqueda
         self.days_max_exp = 39 
-        self.except_days_min_exp = 28
+        self.except_days_min_exp = 26
 
         # PARAMETROS DE ASKBID DE ACCIONES
         self.max_askbid_venta_prom = 0.03
