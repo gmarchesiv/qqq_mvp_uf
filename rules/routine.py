c3c97363c1bc6ee346931898779944eb88f2d6c6
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2025-12-16 15:34:52 -0500

    Update routine.py

--- rules/routine.py
+++ rules/routine.py
@@ -146,7 +146,7 @@ def data_susciption(app, params, vars):
         #     ready += 1
         # if app.options[6]["ASK"] > 0 and app.options[6]["BID"] > 0:
         #     ready += 1
-        if ready == 4:
+        if ready == 6:
             break
 
         time.sleep(0.5)
