eea8e6fd5933ec90c9738ed2d007fd841a829016
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2026-04-16 14:39:01 -0500

    Update wallet.py

--- config/IB/wallet.py
+++ config/IB/wallet.py
@@ -20,7 +20,7 @@ def wallet_config(app, params, vars):
     wallet_load(app, params)
     trade_counter(vars, params)
     money = wallet_cash(app, params)
-    block_Trades(vars, money)
+    # block_Trades(vars, money)
 
 
 def wallet_load(app, params):
